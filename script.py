4e45656e9f649589df28ea879151ed447225900a
HRan2004 <2125764918@qq.com>
2023-02-19 12:06:44 +0800

    feat: 添加目标跟踪相关代码

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 
 from ultralytics import YOLO
 
-
-model = YOLO("weights/yolov8l.pt")
-model.train(data="EacDataInfo.yaml", epochs=260)
-
+# model = YOLO("weights/yolov8l.pt")
+# model.train(data="EacDataInfo.yaml", epochs=260)
 
 
+model = YOLO('weights/v8l-epoch200.pt')
+# model.track(source='assets/0414-1.mp4', save=True)
 
